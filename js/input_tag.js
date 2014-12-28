8052791e1f91964add81f5f8839e0c9cf7fb2202
Cam@mac <camwide88@gmail.com>
2014-12-29 00:49:05 +0800

    let the input field auto fit width

--- js/input_tag.js
+++ js/input_tag.js
@@ -1,31 +1,42 @@
 $.fn.extend({inputtag: function(){
 
 	var self = this;
+	var input_text = self.children('.input-text');
 
-	this.children('.input-text').keydown(function(e){
-	if(e.keyCode == 13){//enter
-		var tag = $('<div class="tag">').text( $(this).val() );
-		var tag_types = ['primary', 'success', 'info', 'warning'];
-		var tag_type_index = Math.floor( Math.random()*(tag_types.length-1) );
-		var tag_type = tag_types[tag_type_index];
-		tag.addClass('tag-'+tag_type);
-		$(this).before(tag);
-		$(this).val('');
-	}
-	if(e.keyCode == 8){//delete
-		if($(this).val().length == 0){
-			$(this).siblings().last().remove();
-		}
-	}
+	self.click(function(){
+		input_text.focus();
 	});
 
-	this.click(function(){
-		$(this).children('.input-text').focus();
-	});
-
-	this.children('.input-text').focus(function(){
+	input_text.focus(function(){
 		self.addClass('focus');
 	}).blur(function(){
 		self.removeClass('focus');
+	});
+
+	input_text.keydown(function(e){
+
+		if(e.keyCode == 13){//enter key: to finish a tag
+			var tag = $('<div class="tag">').text( $(this).val() );
+			var tag_types = ['primary', 'success', 'info', 'warning'];
+			var tag_type_index = Math.floor( Math.random()*(tag_types.length-1) );
+			var tag_type = tag_types[tag_type_index];
+			tag.addClass('tag-'+tag_type);
+			$(this).before(tag);
+			$(this).val('');
+		}
+
+		if(e.keyCode == 8){//delete key: to remove a tag
+			if($(this).val().length == 0){
+				$(this).siblings('.tag').last().remove();
+			}
+		}
+
+	});
+
+	//fit width of the input field
+	input_text.keyup(function(){
+		var width_helper = $('.width-helper');
+		width_helper.text( $(this).val() );
+		$(this).width( width_helper.width()+40 );
 	});
 }});
