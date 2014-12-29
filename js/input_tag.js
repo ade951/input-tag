84e49b6661db49ee2d51fe3a39a74f921ac16ead
Cam@mac <camwide88@gmail.com>
2014-12-29 16:06:22 +0800

    add an input field

--- js/input_tag.js
+++ js/input_tag.js
@@ -2,6 +2,7 @@ $.fn.extend({inputtag: function(){
 
 	var self = this;
 	var input_text = self.children('.input-text');
+	var input_field = self.children('.input-field');
 
 	self.click(function(){
 		input_text.focus();
@@ -16,11 +17,14 @@ $.fn.extend({inputtag: function(){
 	input_text.keydown(function(e){
 
 		if(e.keyCode == 13){//enter key: to finish a tag
-			var tag = $('<div class="tag">').text( $(this).val() );
+			var tag_content = $(this).val();
+
+			var tag = $('<div class="tag">').text( tag_content );
 			var tag_types = ['primary', 'success', 'info', 'warning'];
 			var tag_type_index = Math.floor( Math.random()*(tag_types.length-1) );
 			var tag_type = tag_types[tag_type_index];
 			tag.addClass('tag-'+tag_type);
+
 			$(this).before(tag);
 			$(this).val('');
 		}
@@ -30,6 +34,15 @@ $.fn.extend({inputtag: function(){
 				$(this).siblings('.tag').last().remove();
 			}
 		}
+
+		input_field.val();
+
+		var arr = [];
+		self.children('.tag').each(function(){
+			arr.push($(this).text())
+		});
+		input_field.val( arr.join(',') );
+
 
 	});
 
