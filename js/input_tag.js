da1b3c53d0518c85b199a4784cbac6b6361ec270
Cam@mac <camwide88@gmail.com>
2014-12-30 11:19:41 +0800

    can init from initial values

--- js/input_tag.js
+++ js/input_tag.js
@@ -1,62 +1,91 @@
-$.fn.extend({inputtag: function(){
+$.fn.extend({
+	inputtag: function(){
 
-	var self = this;
-	var input_text = self.children('.input-text');
-	var input_field = self.children('.input-field');
+		var self = this;
+		if( self.attr('initialized') === 'true' ){
+			console.warn('warning: the input-tag already initialized!');
+			return false;
+		}
 
-	self.click(function(){
-		input_text.focus();
-	});
+		self.input_text = self.children('.input-text');
+		self.input_field = self.children('.input-field');
 
-	input_text.focus(function(){
-		self.addClass('focus');
-	}).blur(function(){
-		self.removeClass('focus');
-	});
+		//add a single tag
+		self.tag_add = function(tag_content){
+				var tag = $('<div class="tag">').text( tag_content );
+				var tag_types = ['primary', 'success', 'info', 'warning'];
+				var tag_type_index = Math.floor( Math.random()*(tag_types.length-1) );
+				var tag_type = tag_types[tag_type_index];
+				tag.addClass('tag-'+tag_type);
+				self.input_text.before(tag);
+		}
 
-	input_text.keydown(function(e){
-
-		//enter key or comma: to finish a tag
-		if(e.keyCode == 13 || e.keyCode == 188){
-			
-			e.preventDefault();
-
-			var tag_content = $(this).val();
-			if(!tag_content){
+		//init tags from the input fields value
+		self.tag_init = function(){
+			var tags = self.input_field.val().trim();
+			if( !tags ){
 				return;
 			}
-
-			var tag = $('<div class="tag">').text( tag_content );
-			var tag_types = ['primary', 'success', 'info', 'warning'];
-			var tag_type_index = Math.floor( Math.random()*(tag_types.length-1) );
-			var tag_type = tag_types[tag_type_index];
-			tag.addClass('tag-'+tag_type);
-
-			$(this).before(tag);
-			$(this).val('');
-		}
-
-		if(e.keyCode == 8){//delete key: to remove a tag
-			if($(this).val().length == 0){
-				$(this).siblings('.tag').last().remove();
+			tags = tags.split(',');
+			for(var k in tags){
+				self.tag_add( tags[k] );
 			}
 		}
 
-		input_field.val();
-
-		var arr = [];
-		self.children('.tag').each(function(){
-			arr.push($(this).text())
+		self.click(function(){
+			self.input_text.focus();
 		});
-		input_field.val( arr.join(',') );
+
+		self.input_text.focus(function(){
+			self.addClass('focus');
+		}).blur(function(){
+			self.removeClass('focus');
+		});
+
+		self.input_text.keydown(function(e){
+
+			//enter key or comma: to finish a tag
+			if(e.keyCode == 13 || e.keyCode == 188){
+				
+				e.preventDefault();
+
+				var tag_content = $(this).val();
+				if(!tag_content){
+					return;
+				}
+
+				self.tag_add(tag_content);
+
+				$(this).val('');
+			}
+
+			if(e.keyCode == 8){//delete key: to remove a tag
+				if($(this).val().length == 0){
+					$(this).siblings('.tag').last().remove();
+				}
+			}
+
+			self.input_field.val();
+
+			var arr = [];
+			self.children('.tag').each(function(){
+				arr.push($(this).text())
+			});
+			self.input_field.val( arr.join(',') );
 
 
-	});
+		});
 
-	//fit width of the input field
-	input_text.keyup(function(){
-		var width_helper = $('.width-helper');
-		width_helper.text( $(this).val() );
-		$(this).width( width_helper.width()+40 );
-	});
-}});
+		//fit width of the input field
+		self.input_text.keyup(function(){
+			var width_helper = $('.width-helper');
+			width_helper.text( $(this).val() );
+			$(this).width( width_helper.width()+40 );
+		});
+
+
+		self.tag_init();
+		self.attr('initialized', true);
+		return self;
+	}
+});
